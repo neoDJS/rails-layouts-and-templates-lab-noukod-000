0e2e3e1fd0e71da13e3f89acdf1fc6a34d49cdc5
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-03-26 18:04:41 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'store_admin/home'
+
+  get 'store_admin/orders'
+
   get 'home', to: 'static#home'
   get 'admin/home', to: 'store_admin#home'
   get 'admin/orders', to: 'store_admin#orders'
